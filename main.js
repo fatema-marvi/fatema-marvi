da98c83c102f6029c4bd9146857ed0682a06592e
fatema-marvi <160221766+fatema-marvi@users.noreply.github.com>
2024-02-22 13:36:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+let favoriteColor = "blue";
+console.log(favoriteColor);
+favoriteColor = "green";
+console.log(favoriteColor);
+let username = 'Fatema Marvi';
+let age = 30;
+let Fatema_married = true;
+console.log(username, age, Fatema_married);
---
+++ main.ts
@@ -0,0 +1,9 @@
+let favoriteColor = "blue";
+console.log(favoriteColor)
+favoriteColor ="green";
+console.log(favoriteColor)
+
+let username: string = 'Fatema Marvi';
+let age: number = 30;
+let Fatema_married: boolean = true;
+console.log(username,age,Fatema_married)
